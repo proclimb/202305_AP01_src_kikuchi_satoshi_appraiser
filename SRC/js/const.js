655695f2acbc1a1cb80bbfa5dfe984421971af7a
satoshi kikuchi <202305ap01_kikuchi@x-ide.sakura.ne.jp>
2023-08-03 13:58:39 +0900

    工事管理_仕様書_NO138 による修正
    面積の入力チェックが誤っていることから修正を実施。また、それ以外の入力チェックについてもreturnがないなど誤りがあることが明らかであるので、動作確認済となっている仕入管理の入力チェックのソースを流用して修正を実施。

--- SRC/js/const.js
+++ SRC/js/const.js
@@ -2,40 +2,116 @@
 //工事管理チェック
 //
 function fnConstEditCheck() {
+	//<!-- 8/3日 工事管理_仕様書_NO138 による修正 -->
 	tmp = form.area.value;
-	if (tmp.length > 6 || tmp.match(/[^0-9]+/)) {
+	if (tmp.length > 0 && !tmp.match(/^(\d{0,3}|\d{0,3}\.\d{0,2})$/)) {
 		alert('面積は3桁以内（小数点以下2桁以内）の半角数字で入力してください');
 		return;
 	}
-
-	if (isLengce(100, "築年", form.years))
-	if (isNumericLength(8, "販売予定額", form.sellPrice))
-	if (isNumericLength(9, "内装見越額", JKL.Calendar.Style.prototype.cursor))
-	if (isLength(100, "施工業者", form.constTrader))
-	if (isNumericLength(9, "工事金額", form.constPrice))
-	if (isLength(100, "追加工事", form.constAdd))
-	if (isLength(200, "備考", form.constNote))
-	if (!fnYMDCheck("正しい買取決済日付", form.purchaseDT))
-	if (!fnYMDCheck("正しい工期開始日付", form.workStartDT))
-	if (!fnYMDCheck("正しい工期終了日付", form.workEndDT))
-	if (!fnYMDCheck("正しい電気水道開栓日付", form.lineOpenDT))
-	if (!fnYMDCheck("正しい電気水道閉栓日付", form.lineCloseDT))
-	if (!fnYMDCheck("正しい電気水道開栓連絡日", form.lineOpenContactDT))
-	if (!fnYMDCheck("正しい電気水道閉栓連絡日", form.lineCloseContactDT))
-	if (isLength(200, "備考", form.lineContactNote))
-	if (isLength(100, "電気連絡者", form.electricityCharge))
-	if (isLength(100, "ガス連絡者", form.gasCharge))
-	if (isLength(100, "荷＆鍵引取", form.receive))
-	if (isLength(100, "給湯", form.hotWater))
-	if (fnYMDCheck("正しい現調日付", form.siteDate))
-	if (isLength(100, "届出用紙", form.leavingForm))
-	if (fnYMDCheck("正しい届出期日", form.leavingDT))
-	if (isLength(100, "管理会社", form.manageCompany))
-	if (isLength(100, "管理室", form.floorPlan))
-	if (isLength(100, "前所有者", form.formerOwner))
-	if (isLength(100, "仲介会社（担当）", form.brokerCharge))
-	if (isLength(100, "仲介会社（連絡先）", form.brokerContact))
-	if (isLength(100, "内装担当者", form.interiorCharge))
+	tmp = form.years.value;
+	if (tmp.length > 200) {
+		alert('築年は200文字以内で入力してください');
+		return;
+	}
+	tmp = form.sellPrice.value;
+	if (tmp.length > 9 || tmp.match(/[^0-9]+/)) {
+		alert('販売予定額は9桁以内の半角数字で入力してください');
+		return;
+	}
+	tmp = form.interiorPrice.value;
+	if (tmp.length > 9 || tmp.match(/[^0-9]+/)) {
+		alert('内装見越額は9桁以内の半角数字で入力してください');
+		return;
+	}
+	tmp = form.constTrader.value;
+	if (tmp.length > 100) {
+		alert('施工業者は100文字以内で入力してください');
+		return;
+	}
+	tmp = form.constPrice.value;
+	if (tmp.length > 9 || tmp.match(/[^0-9]+/)) {
+		alert('工事金額は9桁以内の半角数字で入力してください');
+		return;
+	}
+	tmp = form.constAdd.value;
+	if (tmp.length > 100) {
+		alert('追加工事は100文字以内で入力してください');
+		return;
+	}
+	tmp = form.constNote.value;
+	if (tmp.length > 200) {
+		alert('備考は200文字以内で入力してください');
+		return;
+	}
+	if (!fnYMDCheck("正しい買取決済日付", form.purchaseDT)) { return; }
+	if (!fnYMDCheck("正しい工期開始日付", form.workStartDT)) { return; }
+	if (!fnYMDCheck("正しい工期終了日付", form.workEndDT)) { return; }
+	if (!fnYMDCheck("正しい電気水道開栓日付", form.lineOpenDT)) { return; }
+	if (!fnYMDCheck("正しい電気水道閉栓日付", form.lineCloseDT)) { return; }
+	if (!fnYMDCheck("正しい電気水道開栓連絡日", form.lineOpenContactDT)) { return; }
+	if (!fnYMDCheck("正しい電気水道閉栓連絡日", form.lineCloseContactDT)) { return; }
+	tmp = form.lineContactNote.value;
+	if (tmp.length > 200) {
+		alert('備考は200文字以内で入力してください');
+		return;
+	}
+	tmp = form.electricityCharge.value;
+	if (tmp.length > 100) {
+		alert('電気連絡者は100文字以内で入力してください');
+		return;
+	}
+	tmp = form.gasCharge.value;
+	if (tmp.length > 100) {
+		alert('ガス連絡者は100文字以内で入力してください');
+		return;
+	}
+	tmp = form.receive.value;
+	if (tmp.length > 100) {
+		alert('荷＆鍵引取は100文字以内で入力してください');
+		return;
+	}
+	tmp = form.hotWater.value;
+	if (tmp.length > 100) {
+		alert('給湯は100文字以内で入力してください');
+		return;
+	}
+	if (!fnYMDCheck("正しい現調日付", form.siteDate)) { return; }
+	tmp = form.leavingForm.value;
+	if (tmp.length > 100) {
+		alert('届出用紙は100文字以内で入力してください');
+		return;
+	}
+	if (!fnYMDCheck("正しい届出期日", form.leavingDT)) { return; }
+	tmp = form.manageCompany.value;
+	if (tmp.length > 100) {
+		alert('管理会社は100文字以内で入力してください');
+		return;
+	}
+	tmp = form.floorPlan.value;
+	if (tmp.length > 100) {
+		alert('管理室は100文字以内で入力してください');
+		return;
+	}
+	tmp = form.formerOwner.value;
+	if (tmp.length > 100) {
+		alert('前所有者は100文字以内で入力してください');
+		return;
+	}
+	tmp = form.brokerCharge.value;
+	if (tmp.length > 100) {
+		alert('仲介会社（担当）は100文字以内で入力してください');
+		return;
+	}
+	tmp = form.brokerContact.value;
+	if (tmp.length > 100) {
+		alert('仲介会社（連絡先）は100文字以内で入力してください');
+		return;
+	}
+	tmp = form.interiorCharge.value;
+	if (tmp.length > 100) {
+		alert('内装担当者は100文字以内で入力してください');
+		return;
+	}
 
 	if (confirm('この内容で登録します。よろしいですか？')) {
 		form.act.value = 'constEditComplete';
